7b28fa3d1af744b6437c4cd5f8cfbe9c9a7ee4b9
arslanaliabbas <arslanaliabbas271@gmail.com>
2025-02-23 01:46:13 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Arslan26 Bot",
+  botOwner: 'Arslan26 bot', // Your name
+  ownerNumber: '923704200562', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
